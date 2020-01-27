d537c5d34aa434252a8a6df945ea7a36daa6591d
h-hub <harsha.kj89@gmail.com>
2020-01-27 09:16:54 +0530

    add user signup form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from users.forms import SignUpForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
