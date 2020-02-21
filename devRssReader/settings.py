b73152ac06f9dbb30e2140c45a5a27f975479f0a
h-hub <harsha.kj89@gmail.com>
2020-02-21 19:46:00 +0800

    static file fix

--- devRssReader/settings.py
+++ devRssReader/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
