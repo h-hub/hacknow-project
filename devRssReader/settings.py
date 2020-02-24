275832d21334a2e04cdec7f30fba0f6cc210bf1a
h-hub <harsha.kj89@gmail.com>
2020-02-24 19:39:23 +0800

    build fix

--- devRssReader/settings.py
+++ devRssReader/settings.py
@@ -126,17 +126,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'devRssReader/static'),
 )
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
