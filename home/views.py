c30bf82cb68567882b49f696d0faa91088a0829e
h-hub <harsha.kj89@gmail.com>
2020-02-25 18:03:52 +0800

    layout and theme change

--- home/views.py
+++ home/views.py
@@ -13,7 +13,7 @@ import feedparser
 def index(request):
 
     authors = Author.objects.all()
-    paginator = Paginator(authors, 2) 
+    paginator = Paginator(authors, 5) 
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
